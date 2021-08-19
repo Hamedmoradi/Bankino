fb4136c41cc44909e6ce54c19796e6418695a535
hamor <hamedmoradi.mailsbox@gmail.com>
2021-08-19 02:44:39 -0700

    swagger bug fix

--- src/main/java/com/bankino/training/swagger/SwaggerConfig.java
+++ src/main/java/com/bankino/training/swagger/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.bankino.training.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,14 +15,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
 
 @Bean
 public Docket api() {
 	return new Docket(DocumentationType.SWAGGER_2).select()
 			       .apis(RequestHandlerSelectors
-					             .basePackage("net.guides.springboot2.springboot2swagger2.controller"))
-			       .paths(PathSelectors.regex("/.*"))
+					             .basePackage("com.bankino.training.api"))
+			       .paths(PathSelectors.regex("/*.*"))
 			       .build().apiInfo(apiEndPointsInfo());
 }
 private ApiInfo apiEndPointsInfo() {
@@ -32,4 +34,12 @@ private ApiInfo apiEndPointsInfo() {
 			       .version("1.0.0")
 			       .build();
 }
+@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 }
